55fcf0943aa8e7f69821c3fd59b431f14cbded14
simonsobs <simonsobs@example.com>
2021-03-18 19:01:14 +0100

    Rename scripts to acu-headsup and acu-special
    And they get installed properly.

--- setup.py
+++ setup.py
@@ -4,4 +4,5 @@ setup(name='soaculib',
       description='ACU Control Library for Simons Observatory',
       package_dir={'soaculib': 'python'},
       packages=['soaculib'],
+      scripts=['scripts/acu-headsup', 'scripts/acu-special'],
 )
