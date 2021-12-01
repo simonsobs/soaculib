c95cde20707b4b1960b95229fb515f0adc70d427
simonsobs <simonsobs@example.com>
2021-12-01 01:47:58 +0100

    twisted_backend: work on using a single persitent connection
    Attempt at writing a drop-in replacement for
    HTTPConnectionPool... works in that it does not crash, but not obvious
    that it helps resolve our issues with multiple connections and
    persistence.

--- python/twisted_backend.py
+++ python/twisted_backend.py
@@ -5,7 +5,7 @@ import soaculib
 
 from twisted.internet import reactor
 from twisted.internet.defer import (
-    inlineCallbacks, Deferred, DeferredList, returnValue)
+    inlineCallbacks, Deferred, DeferredList, DeferredLock, returnValue)
 import twisted.web.client as tclient
 from twisted.web.http_headers import Headers
 
@@ -29,8 +29,18 @@ class TwistedHttpBackend(soaculib._Backend):
         self.return_val_func = returnValue
 
         if web_agent is None:
-            if persistent:
+            if persistent is True:  # convert true to the classic default...
+                persistent = 'pool2'
+            if persistent == 'single':
+                pool = SinglePersistentConnection(reactor)
+                web_agent = tclient.Agent(reactor, pool=pool)
+            elif persistent == 'pool1':
                 pool = tclient.HTTPConnectionPool(reactor)
+                pool.maxPersistentPerHost = 1
+                web_agent = tclient.Agent(reactor, pool=pool)
+            elif persistent == 'pool2':
+                pool = tclient.HTTPConnectionPool(reactor)
+                pool.maxPersistentPerHost = 2
                 web_agent = tclient.Agent(reactor, pool=pool)
             else:
                 web_agent = tclient.Agent(reactor)
@@ -74,3 +84,52 @@ class TwistedHttpBackend(soaculib._Backend):
         d = Deferred()
         reactor.callLater(delay, d.callback, None)
         yield d
+
+
+class SinglePersistentConnection:
+    """This "pool" is based on twisted.client.HTTPConnectionPool, and is
+    supposed to stand in for it.  Instead of managing multiple
+    connections, this enforces that a single connection be used for
+    all communication with the server.
+
+    """
+
+    _factory = tclient._HTTP11ClientFactory
+
+    def __init__(self, reactor):
+        self.persistent = True
+        self._locks = {}
+        self._connections = {}
+
+    def getConnection(self, key, endpoint):
+        if key in self._locks:
+            return self._locks[key].acquire().addCallback(
+                lambda lock: self._connections[key])
+        self._locks[key] = DeferredLock()
+        def quiescentCallback(protocol):
+            self._locks[key].release()
+        factory = self._factory(quiescentCallback, repr(endpoint))
+        cd = endpoint.connect(factory)
+        def just_the_prot(results):
+            ok, connection = results[0]
+            if ok:
+                self._connections[key] = connection
+                return connection
+            else:
+                del self._locks[key]
+                e.subFailure.raiseException()
+        return DeferredList([cd, self._locks[key].acquire()]).addCallback(just_the_prot)
+
+    def closeCachedConnections(self):
+        """
+        Close all persistent connections.
+
+        @return: L{defer.Deferred} that fires when all connections have been
+            closed.
+        """
+        results = []
+        self._locks = {}
+        for key, conn in self._connections.items():
+            results.append(conn.abort())
+        self._connections = {}
+        return defer.gatherResults(results).addCallback(lambda ign: None)
