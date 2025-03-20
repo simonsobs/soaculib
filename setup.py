2b550bac9b362129df90c483a135084f03e828c1
Matthew Hasselfield <mhasself@users.noreply.github.com>
2025-03-20 17:48:20 +0000

    Move to pyproject.toml with minimal setup.py

--- setup.py
+++ setup.py
@@ -3,30 +3,6 @@ from setuptools import setup
 import versioneer
 
 setup(name='soaculib',
-      description='ACU Control Library for Simons Observatory',
       version=versioneer.get_version(),
       cmdclass=versioneer.get_cmdclass(),
-      package_dir={'soaculib': 'python'},
-      packages=['soaculib', 'soaculib.cli'],
-      entry_points={
-          'console_scripts': [
-              'acu-headsup=soaculib.cli.headsup:main',
-              'acu-ftp=soaculib.ftptool:main',
-              'acu-special=soaculib.cli.tool:main',
-          ],
-      },
-      package_data={
-          'soaculib': ['acu-configs.yaml']
-      },
-      install_requires=[
-           'requests',
-           'twisted',
-      ],
-      extras_require={
-          'simulator': [
-              'flask',
-              'numpy',
-              'scipy',
-          ],
-      },
       )
